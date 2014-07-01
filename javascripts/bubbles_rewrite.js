e2d66c0c0decc5ae5838b35c50c1441074acf45a
Yaniv Savir <saviry@gmail.com>
2014-07-01 15:12:54 -0400

    various improvements in rewrite

--- javascripts/bubbles_rewrite.js
+++ javascripts/bubbles_rewrite.js
@@ -1,16 +1,18 @@
 var Bubble = {
+  version: 0.2,
   make: function(options){
     // bubble defaults
     var bubble = {
-      size: "100px",
+      size: 100,
       color: "pink",
       klass: 'bubble',
-      parent: 'body',
-      offsetX: "0px",
-      offsetY: "0px",
+      parent: document.getElementsByTagName('body')[0],
+      offsetX: 0,
+      offsetY: 0,
       drift: true,
-      verticalDriftRate: 4, // negative will cause downward drifting
-      horizontalDriftRate: 4
+      verticalSpeed: 20, // negative will cause downward drifting
+      horizontalSpeed: 20,
+      driftRate: 900
     };
     // add some functionality:
     Object.defineProperties(bubble, {
@@ -19,7 +21,7 @@ var Bubble = {
         value: function(){
           this.render();
           // set listener for pop
-          $(this.bubble).on("click", function(){
+          this.bubble.addEventListener("click", function(){
             this.pop();
           }.bind(this));
           // Unless drift is false, call the drfit function corresponding
@@ -33,14 +35,15 @@ var Bubble = {
         value: function(){
           // append the template to the DOM and save the element as
           // this.bubble
-          this.bubble = $(this.template()).appendTo(this.parent);
+          this.parent.insertAdjacentHTML('beforeend', this.template());
+          this.bubble = this.parent.lastChild
         }
       },
       template: {
         value: function(){
           var bubbleTemplate = [];
 
-          if (this.url) bubbleTemplate.push('<a href="', this.url, '" target="_blank">');
+          // if (this.url) bubbleTemplate.push('<a href="', this.url, '" target="_blank">');
           // push all standard info
           bubbleTemplate.push('<div style="color:white;background-color:white;',
                                 // gradient breakdowns
@@ -53,12 +56,12 @@ var Bubble = {
                                 // IE
                                 'background:-ms-radial-gradient(35% 35%, circle cover, #ffffff 0%, ', this.color, ' 25%, ', this.color, ' 60%, #ffffff 80%);',
                                 // Size
-                                'width:', this.size,
-                                ';height:',this.size,
+                                'width:', this.size + "px",
+                                ';height:', this.size + "px",
                                 // Positioning
                                 ';position:absolute;',
-                                'top:', this.offsetY,
-                                ';left:', this.offsetX,
+                                'top:', this.offsetY + "px",
+                                ';left:', this.offsetX + "px",
                                 // border
                                 ';border:1px solid #eeeeee;border-radius:1000px;text-align:center;"',
                                 // class
@@ -68,25 +71,29 @@ var Bubble = {
           // push div close
           bubbleTemplate.push('</div>');
           // push anchor tag close if bubble has a url
-          if (this.url) bubbleTemplate.push('</a>');
+          // if (this.url) bubbleTemplate.push('</a>');
           return bubbleTemplate.join("");
         }
       },
       pop: {
         value: function(){
-          var bubble = $(this.bubble);
-          bubble.css({
+          this.bubble.css({
             // make the bubble invisible
             "background": "none",
             "border": "none"
           })
           // add that crappy ascii for popping image
-          bubble.append('<div class="popContainer" style="color:black"><div class="baloonPOP">\\</div><div class="baloonPOP">|</div><div class="baloonPOP">/</div><div class="baloonPOP">-</div><div class="baloonPOP">o</div><div class="baloonPOP">-</div><div class="baloonPOP">/</div><div class="baloonPOP">|</div><div class="baloonPOP">\\</div></div>')
-          $('.popContainer').css({
-            "width": "90px",
-            "height": "90px",
-            "position": "relative"
-          });
+          this.bubble.append('<div class="popContainer" style="color:black"><div class="baloonPOP">\\</div><div class="baloonPOP">|</div><div class="baloonPOP">/</div><div class="baloonPOP">-</div><div class="baloonPOP">o</div><div class="baloonPOP">-</div><div class="baloonPOP">/</div><div class="baloonPOP">|</div><div class="baloonPOP">\\</div></div>')
+          popContainers.forEach(function(popContainer){
+            popContainer.style.width = "90px";
+            popContainer.style.height = "90px";
+            popContainer.style.position = "relative";
+          })
+          // $('.popContainer').css({
+          //   "width": "90px",
+          //   "height": "90px",
+          //   "position": "relative"
+          // });
           // assign sizes to the divs from above
           $('.baloonPOP').css({
             "width": "30px",
@@ -96,24 +103,42 @@ var Bubble = {
             "display": "inline-block"
           });
           // remove the bubble entirely
-          setTimeout(function(){ bubble.detach() }, 100)
+          setTimeout(function(){ this.bubble.detach() }.bind(this), 100)
         }
       },
       driftBubble: {
+        var remainingMovementY = this.directionY;
+        var remainingMovementX = this.directionX;
+        var tick = function(){
+          this.bubble.style.top = 
+        }
+
+
+
         // Make that bubble move.
         // Direction along x axis determined randomly each time.
         value: function(){
-          var directionY = "-=" + 5 * this.verticalDriftRate;
-          var directionX = "+=" + (5 * (Math.round(Math.random() * 100) % 2 === 0 ? 1 : -1)) * this.horizontalDriftRate;
+          var directionY = "-=" + this.verticalSpeed;
+          var directionX = "+=" + ((Math.round(Math.random() * 100) % 2 === 0 ? 1 : -1)) * this.horizontalSpeed;
+          var makeItDrift = function(){
+            var bubbleStyle = this.bubble.style
+            bubbleStyle.top = bubbleStyle.top + directionY
+            bubbleStyle.left = bubbleStyle.left + directionX
+          }
+          
           this.bubble.animate({
             top: directionY,
             left: directionX
             },
-            900,
+            this.driftRate,
             "linear",
             // Never let it stop
             function(){
-              this.driftBubble();
+              if (this.bubble.css("top") < -100) {
+                this.bubble.detach();
+              } else {
+                this.driftBubble();
+              }
             }.bind(this)
           );
         }
